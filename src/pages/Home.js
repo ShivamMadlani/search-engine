e9a7154a5ba6374f74e6a110a218e7746755a7c8
Shivam <shivammadlani5@gmail.com>
2022-09-03 10:27:05 +0530

    home page

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <>
+            <div className='header'>
+                header
+            </div>
+            <div className='body'>
+            </div>
+        </>
+    );
+}
+
+export default Home;
