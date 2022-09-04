7603d7cee1fa943ef0554a6c5d53be250920dca1
Shivam <shivammadlani5@gmail.com>
2022-09-04 16:00:50 +0530

    search results

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -59,11 +59,41 @@ const SearchPage = () => {
 						</div>
 					</div>
 				</div>
-
 			</div>
-			<div className="srchpg-results">
 
-			</div>
+
+			{term && (
+				<div className="srchpg-results">
+					<p className="searchPage__resultCount">
+						About {data?.searchInformation.formattedTotalResults} results (
+						{data?.searchInformation.formattedSearchTime} seconds) for{" "}
+						<strong>{term}</strong>
+					</p>
+
+					{data?.items.map((item) => (
+						<div className="searchPage__result">
+							<a className="searchPage__resultLink" href={item.link}>
+								{item.pagemap?.cse_image?.length > 0 &&
+									item.pagemap?.cse_image[0]?.src && (
+										<img
+											className="searchPage__resultImage"
+											src={
+												item.pagemap?.cse_image?.length > 0 &&
+												item.pagemap?.cse_image[0]?.src
+											}
+											alt=""
+										/>
+									)}
+								{item.displayLink} ▾
+							</a>
+							<a className="searchPage__resultTitle" href={item.link}>
+								<h2>{item.title}</h2>
+							</a>
+
+							<p className="searchPage__resultSnippet">{item.snippet}</p>
+						</div>
+					))}
+				</div>)}
 		</div>
 	);
 }
